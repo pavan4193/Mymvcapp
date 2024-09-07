08d68c8db9bec7a3e8ef8497fba516b25815fa5e
pavan4193 <75196752+pavan4193@users.noreply.github.com>
2024-09-07 18:11:14 -0500

    category

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+namespace MyMvcApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set;}
+        public string Name { get; set;}
+        
+        public int DisplayOrder { get; set;}
+    }
+}
