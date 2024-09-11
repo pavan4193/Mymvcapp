5db6af38ee78b5599d5c912a2f24a821e2ba32c5
pavan4193 <75196752+pavan4193@users.noreply.github.com>
2024-09-10 22:11:58 -0500

    Dbcontext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MyMvcApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Add DbSets for your entities here, for example:
+        // public DbSet<MyEntity> MyEntities { get; set; }
+    }
+}
