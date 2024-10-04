eff851a76481b9700834f62c8822c2d05255916a
pavan4193 <75196752+pavan4193@users.noreply.github.com>
2024-10-03 20:10:01 -0500

    Toastr Notification

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4e45e717838af5c6b9370917bcd1518e5b7823")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86924f320648f120fe21773d2fdd72f043444e65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
