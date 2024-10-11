101770035100c363ff19ae0d8246b33303cfcae8
pavan4193 <75196752+pavan4193@users.noreply.github.com>
2024-10-11 11:01:09 -0500

    mc

--- MyMvcApp.Models/obj/Debug/net8.0/MyMvcApp.Models.AssemblyInfo.cs
+++ MyMvcApp.Models/obj/Debug/net8.0/MyMvcApp.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dae708381b12909c87b7fedef2a07d9b3ead4d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d53b448092d62050b335fa324cafd1578a6dc6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
