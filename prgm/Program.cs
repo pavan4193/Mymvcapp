5d2316ed6c504edee01ef05ea60058157626b162
pavan4193 <75196752+pavan4193@users.noreply.github.com>
2024-11-13 21:08:48 -0600

    mc

---
+++ prgm/Program.cs
@@ -0,0 +1,23 @@
+﻿int n;
+Console.WriteLine("Enter n value");
+n=Convert.ToInt32(Console.ReadLine());
+
+
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n-i-1;j++)
+    {
+        Console.WriteLine(" ");
+    }
+
+    for(int j=0;j<2*i+1;j++)
+    {
+        Console.WriteLine("*");
+    }
+
+    for(int j=0;j<n-i-1;j++)
+    {
+        Console.WriteLine(" ");
+    }
+
+}
